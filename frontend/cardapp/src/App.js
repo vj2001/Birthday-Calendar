f59574013762a9b1a9a372aa60b3138654cd603a
vj2001 <vishaljain63957@gmail.com>
2022-04-15 13:23:05 +0530

    UI changes done

--- frontend/cardapp/src/App.js
+++ frontend/cardapp/src/App.js
@@ -1,31 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import Card from "./Components/Card/Card";
-import Button from "./Components/Button/Button";
 import Form from "./Components/Form/Form";
-import {useSelector,useDispatch} from "react-redux"
+// import {useSelector,useDispatch} from "react-redux"
 import "./App.css"
 
 function App() {
-
-  const dispatch = useDispatch()
-  const {users}= useSelector((state)=>state)
-
-  const deleteUser=(e)=>{
-    console.log("I am deleted")
-      e.preventDefault()
-      users.pop()
-      dispatch({type:"users",value:users})
-  }
-  
   return (
     <div className="App">
       <Form/>
-      <h3>HAPPY BIRTHDAY !!</h3>
-      <Card/>
-      {users.length>0?
-      <Button type="button" onClick={deleteUser}>DELETE</Button>
-      :<alert bg-info>Please add some Event</alert>}
-      
+      <Card/>      
     </div>
   );
 }
--- frontend/cardapp/src/Components/Card/Card.js
+++ frontend/cardapp/src/Components/Card/Card.js
@@ -1,23 +1,41 @@
-import { useSelector } from "react-redux"
+import { useSelector,useDispatch } from "react-redux"
+import Button from "../Button/Button";
 import "./Card.css"
 // import img from './images/pic1.jpeg'
 
 const Card =()=>{
-    const {users} = useSelector((state)=>state)
+    const dispatch = useDispatch()
+    const {users}= useSelector((state)=>state)
+  
+    
+    const deleteUser=(e)=>{
+        console.log("I am deleted")
+          e.preventDefault()
+          users.pop()
+          dispatch({type:"users",value:users})
+      }
+
     // console.log(users)
 
     return(
         <div>
+            <h3 class="heading">HAPPY BIRTHDAY !!</h3>
+            <p class="description">{users.length} birthdays Today</p>
+
         {users.map((user)=>(
-            <div key={user.id} className="card">
+            <article key={user.id} className="card">
             <img src={require(`../../images/${user.pic}`)}/>
             <ul className="list-group list-group-flush my-2">
                 <li className="list-group-item">{user.username}</li>
-                <li className="list-group-item">{user.userage}</li>
+                <li className="list-group-item">{user.userage} years</li>
             </ul>
-            </div>
+            </article>
             
         ))}
+
+        {users.length>0?
+                 <Button type="button" onClick={deleteUser}>DELETE</Button>
+                             :<alert bg-info>Please add some Event</alert>}
         </div>
     )
 }
